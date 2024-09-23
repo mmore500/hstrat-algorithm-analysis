be994db9d76ba6030ace87b1d98347b224e3ca2c
mmore500 <mmore500.login@gmail.com>
2024-09-23 02:26:28 +0000

    Deploying to binder from @ mmore500/hstrat-algorithm-analysis@f3dff74f74e133c664c90ce7e7d5a00d13fd0806 🚀

--- replit.nix
+++ replit.nix
@@ -1,13 +1,14 @@
 { pkgs }: {
     deps = [
+      pkgs.python311Packages.pytest
+      pkgs.python310Packages.pytest
       pkgs.vim
       pkgs.pandoc
       pkgs.texlive.combined.scheme-full
       pkgs.python39Packages.pip
-      pkgs.nodejs
       pkgs.editorconfig-checker
       pkgs.python39Packages.editorconfig
-      pkgs.nodejs-16_x
       pkgs.cowsay
+      pkgs.bibtex-tidy
     ];
 }
